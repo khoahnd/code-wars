61b7542825ed89e6eb9aab1519879b45d917b744
Khoa Ho <khoa.ho@dirox.net>
2020-12-30 18:16:52 +0700

    title-case

--- python/create-phone-number.py
+++ python/create-phone-number.py
@@ -9,4 +9,20 @@ Don't forget the space after the closing parentheses!
 """
 
 def create_phone_number(n):
-    #your code here
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+# ------------------------------------------------------------Testing----------------------------------------------------
+
+Test.describe("Basic tests")
+Test.assert_equals(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+Test.assert_equals(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111")
+Test.assert_equals(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+Test.assert_equals(create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]), "(023) 056-0890")
+Test.assert_equals(create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), "(000) 000-0000")
+
+
+# ------------------------------------------------------------Other Solution---------------------------------------------
+
+def create_phone_number(n):
+    n = ''.join(map(str,n))
+    return '(%s) %s-%s'%(n[:3], n[3:6], n[6:])
